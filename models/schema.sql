1cb70c98c87512a03d83667683dcf88429bf08dc
Erik Salvatier <m.erik.salvatier@gmail.com>
2018-11-26 20:12:15 -0800

    basic html

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,39 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS eventOrganizersDB;
+CREATE DATABASE eventOrganizersDB;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+CREATE TABLE events (
+  id INT AUTO_INCREMENT,
+  event_time DATE NOT NULL,
+  strt_time TIMESTAMP NOT NULL,
+  end_time TIMESTAMP,
+  loc VARCHAR(255) NOT NULL,
+  event_type VARCHAR(255) NOT NULL,
+  calenderObj JSON NOT NULL,
+  canceled BOOLEAN NOT NULL DEFAULT FALSE,
+  FOREIGN KEY event_owner REFERENCES users(uid),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE users (
+  id INT AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  friends VARCHAR(255),
+  uid VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+/* id INT AUTO_INCREMENT,
+  title VARCHAR(255) NOT NULL,
+  allDay BOOLEAN NOT NULL DEFAULT FALSE,
+  strt_time TIMESTAMP NOT NULL,
+  end_time TIMESTAMP,
+  url VARCHAR(255),
+  className VARCHAR(255),
+  editable,
+  startEditable,
+  durationEditable,
+  resourceEditable,
+  render,
+  overlap,
+  constrain
+  */
