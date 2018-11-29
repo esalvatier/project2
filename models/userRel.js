771e0bdc7cc4ba4abc4838ebe89a1a6ed8bdc4ce
Erik Salvatier <m.erik.salvatier@gmail.com>
2018-11-28 18:00:29 -0800

    pretiffying

--- models/userRel.js
+++ models/userRel.js
@@ -1,27 +1,31 @@
-module.exports = function (sequelize, DataTypes) {
-  var userRelationship = sequelize.define("userRelationship", {
-    relID: {
-      type: DataTypes.INTEGER,
-      primary: true,
-      autoIncrement: true
+module.exports = function(sequelize, DataTypes) {
+  var userRelationship = sequelize.define(
+    "userRelationship",
+    {
+      relID: {
+        type: DataTypes.INTEGER,
+        primary: true,
+        autoIncrement: true
+      },
+      fromUser: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      targetUser: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      statusCode: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 1
+      }
     },
-    fromUser: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    targetUser: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    statusCode: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 1
+    {
+      timestamps: true,
+      createdAt: "sentTime",
+      updatedAt: "responseTime"
     }
-  }, {
-    timestamps: true,
-    createdAt: "sentTime",
-    updatedAt: "responseTime"
-  });
+  );
   return userRelationship;
 };
