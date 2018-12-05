93ceee700bef3efeff80d0ee74101e36636ed291
Dmitrii Zakharov <dzakharov@splunk.com>
2018-12-04 21:15:15 -0800

    Retrieving users from db

--- public/js/friends.js
+++ public/js/friends.js
@@ -1,81 +1,49 @@
 // Get references to page elements
-var $friends = $("#friends");
+var $friendsList = $(".friends");
 
 // The API object contains methods for each kind of request we'll make
 var API = {
-  getFriends: function() {
+  getFriends: function(id) {
     return $.ajax({
-      url: "api/friend/:uid",
-      type: "GET",
-      uid: uid
+      url: "api/friend/" + id,
+      type: "GET"
     });
   }
 };
 
-// refreshExamples gets new examples from the db and repopulates the list
-var refreshExamples = function() {
-  API.getExamples().then(function(data) {
-    var $examples = data.map(function(example) {
+// refreshFriends gets new friends from the db and repopulates the list
+var refreshFriends = function() {
+  console.log("BEFORE API CALL");
+  API.getFriends("0GE3xvI4fHTbsWH5ZebWMoBVM5v2").then(function(data) {
+
+    console.log("data: " + data);
+
+    var $friends = data.map(function(friend) {
+
+      console.log("friend: " + friend);
+      console.log("friend.firstName: " + friend.firstName);
+      
+
       var $a = $("<a>")
-        .text(example.text)
-        .attr("href", "/example/" + example.id);
+        .text(friend.firstName)
 
       var $li = $("<li>")
         .attr({
           class: "list-group-item",
-          "data-id": example.id
+          "data-id": friend.id
         })
         .append($a);
-
-      var $button = $("<button>")
-        .addClass("btn btn-danger float-right delete")
-        .text("ｘ");
-
-      $li.append($button);
-
       return $li;
-    });
+      });
 
-    $exampleList.empty();
-    $exampleList.append($examples);
+    console.log("Before append");
+    console.log("$friendsList: " + $friendsList);
+    console.log("$friends.data: " + $friends);
+    //$friendsList.empty();
+    //$("#friends-display").html("BLAHBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB");
+    $("#friends").append($friends);
+    //$("#friends-display").hide();
   });
 };
 
-// handleFormSubmit is called whenever we submit a new example
-// Save the new example to the db and refresh the list
-var handleFormSubmit = function(event) {
-  event.preventDefault();
-
-  var example = {
-    text: $exampleText.val().trim(),
-    description: $exampleDescription.val().trim()
-  };
-
-  if (!(example.text && example.description)) {
-    alert("You must enter an example text and description!");
-    return;
-  }
-
-  API.saveExample(example).then(function() {
-    refreshExamples();
-  });
-
-  $exampleText.val("");
-  $exampleDescription.val("");
-};
-
-// handleDeleteBtnClick is called when an example's delete button is clicked
-// Remove the example from the db and refresh the list
-var handleDeleteBtnClick = function() {
-  var idToDelete = $(this)
-    .parent()
-    .attr("data-id");
-
-  API.deleteExample(idToDelete).then(function() {
-    refreshExamples();
-  });
-};
-
-// Add event listeners to the submit and delete buttons
-$submitBtn.on("click", handleFormSubmit);
-$exampleList.on("click", ".delete", handleDeleteBtnClick);
+refreshFriends();
