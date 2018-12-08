5f204adf0c8531566cb5e5096978ae57579a87f9
Erik Salvatier <m.erik.salvatier@gmail.com>
2018-12-08 08:00:37 -0800

    freint searching fix

--- public/js/search.js
+++ public/js/search.js
@@ -1,53 +1,55 @@
-$(document).on("click", "#search-button", function(event) {
-  event.preventDefault();
+$(document).ready(function() {
+  $(document).on("click", "#search-button", function(event) {
+    event.preventDefault();
 
-  // Grabs user input
-  var searchterm = $("#search-input")
-    .val()
-    .trim();
+    // Grabs user input
+    var searchterm = $("#search-input")
+      .val()
+      .trim();
 
-  // var message = $("#message-input")
-  //   .val()
-  //   .trim();
+    // var message = $("#message-input")
+    //   .val()
+    //   .trim();
 
-  $.ajax("/api/search/", {
-    method: "GET",
-    data: {
-      searchterm: searchterm
-    }
-  }).then(function(response) {
-    $("#friends-found").empty();
-    for (var i = 0; i < response.length; i++) {
-      var result = $("<li>").text(
-        "Name: " +
-          response[i].firstName +
-          " " +
-          response[i].lastName +
-          " Email: " +
-          response[i].email
-      );
-      var newBtn = $("<button>")
-        .attr({
-          class: "frndRequestBtn btn btn-secondary",
-          uid: response[i].uid,
-          display: "inline-block"
-        })
-        .text("Send Friend Request");
-      result.append(newBtn);
-      $("#friends-found").append(result);
-    }
+    $.ajax("/api/search/", {
+      method: "GET",
+      data: {
+        searchterm: searchterm
+      }
+    }).then(function(response) {
+      $("#friends-found").empty();
+      for (var i = 0; i < response.length; i++) {
+        var result = $("<li>").text(
+          "Name: " +
+            response[i].firstName +
+            " " +
+            response[i].lastName +
+            " Email: " +
+            response[i].email
+        );
+        var newBtn = $("<button>")
+          .attr({
+            class: "frndRequestBtn btn btn-secondary",
+            uid: response[i].uid,
+            display: "inline-block"
+          })
+          .text("Send Friend Request");
+        result.append(newBtn);
+        $("#friends-found").append(result);
+      }
+    });
   });
-});
 
-$(document).on("click", ".frndRequestBtn", function() {
-  console.log("request");
-  var targetUser = $(this).attr("uid");
-  var fromUser = sessionStorage.getItem("localUID");
-  $.ajax("/api/friend", {
-    method: "POST",
-    data: { fromUser: fromUser, targetUser: targetUser, status: 1 }
+  $(document).on("click", ".frndRequestBtn", function() {
+    console.log("request");
+    var targetUser = $(this).attr("uid");
+    var fromUser = sessionStorage.getItem("localUID");
+    $.ajax("/api/friend", {
+      method: "POST",
+      data: { fromUser: fromUser, targetUser: targetUser, status: 1 }
+    });
+    $(this)
+      .parent()
+      .remove();
   });
-  $(this)
-    .parent()
-    .remove();
 });
